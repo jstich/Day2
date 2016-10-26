6fdff9d23e26bd56e9d9b8a501baed6408e6d1f7
Adam Lumbar <adlumbar@gmail.com>
2016-10-26 10:08:04 -0500

    another one

--- MySolution/Project1/Program.cs
+++ MySolution/Project1/Program.cs
@@ -14,6 +14,11 @@ namespace Project1
             Console.WriteLine("change 1");
             Console.WriteLine("change 2 ");
             Console.WriteLine("change 3");
+            Console.WriteLine("this is a change pussy");
+            Console.WriteLine("this is another one (dj kahlid voice");
+            Console.WriteLine("another one ");
+            Console.WriteLine("hey");
+
         }
     }
 }
